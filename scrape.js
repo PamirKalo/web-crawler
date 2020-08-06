cea91e9c418687fcd1e661c03b886c9e31c3ec66
PamirKalo <kbm2@mail.bg>
2020-08-06 13:24:34 +0300

    Update scrape.js with new url and fix bugs

--- scrape.js
+++ scrape.js
@@ -5,41 +5,51 @@ const {
 const jQueryInit = require('jquery');
 
 const urlCarsBg = 'https://www.cars.bg/?brandId=38&currencyId=1&fromhomeu=1&models%5B0%5D=591&go=cars&offersFor1=2&offersFor4=1&stateId=1&search=2&cref=107&page=1&cref=107';
-const urlAutoBg = 'https://www.auto.bg/pcgi/results.cgi?cat=1&page=1&searchres=gecbiuq1&marka=Kia&model=Ceed';
+const urlAutoBg = 'https://www.auto.bg/obiavi/avtomobili-dzhipove/kia/ceed';
 
 // cars.bg:
 const getAdsFromCarsBg = async (url, arr) => {
-    const dom = await JSDOM.fromURL(url);
-    const $ = jQueryInit(dom.window);
+    const dom = await JSDOM.fromURL(url); // create DOM
+    const $ = jQueryInit(dom.window); // create JQuery model
 
-    if ($('.odd,.even').length === 0) {
+    if ($('.odd,.even').length === 0) { // symptoms for the last ad page
         return arr;
     }
+
+    // essentially scraping:
     const adsFromCurrentPage = [...$('.odd,.even')]
         .map((tr) => {
             const title = $(tr).find('.ver15black b').html();
-            const price = $(tr).find('.ver20black strong').html();
+            const price = $(tr).find('.ver20black strong').html() || 'N/A';
             const year = $(tr).find('.year').html();
             const provider = 'cars.bg';
             return [title, price + ' лв', year + ' год', provider];
         });
     arr.push(...adsFromCurrentPage);
+
+    // finding next page url:
     const pageRegex = /(page=)(\d+)/;
     const nextPageUrl = url.replace(pageRegex, (match, p1, p2) => {
         const nextPage = Number(p2) + 1;
         return p1 + nextPage;
     });
+
+    // recursive call:
     await getAdsFromCarsBg(nextPageUrl, arr);
     return arr;
 };
 
-// auto.bg
+// auto.bg:
 const getAdsFromAutoBg = async (url, arr) => {
-    const dom = await JSDOM.fromURL(url);
-    const $ = jQueryInit(dom.window);
-    if ($('.resultItem').length === 0) {
+    const dom = await JSDOM.fromURL(url); // create DOM
+    const $ = jQueryInit(dom.window); // create JQuery model
+
+    if ($('.resultItem').length === 0) { // symptoms for the last ad page
+        console.log('alooooo');
         return arr;
     }
+
+    // content scraping:
     const pageAdsNames = [...$('.resultItem')]
         .map((ad) => {
             const title = $(ad).find('.link a').html();
@@ -50,11 +60,16 @@ const getAdsFromAutoBg = async (url, arr) => {
             return [title, price + ' лв', year + ' год', provider];
         });
     arr.push(...pageAdsNames);
-    const pageRegex = /(page=)(\d+)/;
-    const nextPageUrl = url.replace(pageRegex, (match, p1, p2) => {
+
+    // finding next page url:
+    const pageRegex = /(page\/)(\d+)/;
+    let nextPageUrl = url.replace(pageRegex, (match, p1, p2) => {
         const nextPage = Number(p2) + 1;
         return p1 + nextPage;
     });
+    if (pageRegex.test(nextPageUrl) === false) nextPageUrl = url + '/page/1';
+
+    // recursive call:
     await getAdsFromAutoBg(nextPageUrl, arr);
     return arr;
 };
